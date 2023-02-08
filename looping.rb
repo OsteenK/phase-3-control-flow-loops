1c953ae5d1927e0dfd6b47be34225cab5e485216
osteen <osteen.mutinda@student.moringaschool.com>
2023-02-08 09:48:34 +0300

    Completed

--- looping.rb
+++ looping.rb
@@ -1,5 +1,9 @@
 def happy_new_year
   # your code here
+  10.downto(1) do |i|
+    puts i
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +21,24 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+    end
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed = ''
+  str.chars.each do |char|
+    reversed = char + reversed
+  end
+  return reversed
 end
